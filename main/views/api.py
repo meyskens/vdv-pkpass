4870c6a022d5545199dd70c3581a08ab55bf9e16
Q Misell <q@magicalcodewit.ch>
2025-02-05 12:56:08 +0000

    image upload API

--- main/views/api.py
+++ main/views/api.py
@@ -3,7 +3,7 @@ import base64
 import binascii
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from .. import ticket
+from .. import ticket, aztec
 
 
 @csrf_exempt
@@ -26,6 +26,49 @@ def upload_aztec(request):
         barcode_data = base64.b64decode(data["barcode_data"])
     except binascii.Error:
         return HttpResponse(status=400)
+
+    try:
+        ticket_obj = ticket.update_from_subscription_barcode(barcode_data, account=None)
+    except ticket.TicketError as e:
+        return HttpResponse(json.dumps({
+            "title": e.title,
+            "message": e.message,
+            "exception": e.exception,
+        }), status=422, content_type="application/json")
+
+    return HttpResponse(json.dumps({
+        "ticket_id": ticket_obj.id,
+        "access_token": ticket_obj.pkpass_authentication_token
+    }), content_type="application/json")
+
+
+@csrf_exempt
+def upload_aztec_img(request):
+    if request.method != 'POST':
+        return HttpResponse(status=405)
+
+    if request.content_type != 'multipart/form-data':
+        return HttpResponse(status=400)
+
+    file = request.FILES["photo"]
+    if file.size > 16 * 1024 * 1024:
+        return HttpResponse(json.dumps({
+            "title": "Too large",
+            "message": "The photo must be less than 16MB"
+        }), status=422, content_type="application/json")
+    elif file.content_type not in ("image/jpeg", "image/png"):
+        return HttpResponse(json.dumps({
+            "title": "Unsupported",
+            "message": "The photo must be a JPEG or PNG"
+        }), status=422, content_type="application/json")
+
+    try:
+        barcode_data = aztec.decode(file.read())
+    except aztec.AztecError as e:
+        return HttpResponse(json.dumps({
+            "title": "Unable to decode",
+            "message": str(e)
+        }), status=422, content_type="application/json")
 
     try:
         ticket_obj = ticket.update_from_subscription_barcode(barcode_data, account=None)
