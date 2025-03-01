01ec22bfe9c685c5641786bc7b91457cbec4b19c
Q Misell <q@magicalcodewit.ch>
2025-03-01 16:34:47 +0100

    change sharing perms on google classes, take 2

--- main/management/commands/sync-gwallet-classes.py
+++ main/management/commands/sync-gwallet-classes.py
@@ -107,7 +107,7 @@ class Command(BaseCommand):
                 "securityAnimation": {
                     "animationType": "foilShimmer"
                 },
-                "multipleDevicesAndHoldersAllowedStatus": "MULTIPLE_USERS"
+                "multipleDevicesAndHoldersAllowedStatus": "MULTIPLE_HOLDERS"
             }
         ).execute()
         generic_class.update(
@@ -196,7 +196,7 @@ class Command(BaseCommand):
                 "securityAnimation": {
                     "animationType": "foilShimmer"
                 },
-                "multipleDevicesAndHoldersAllowedStatus": "MULTIPLE_USERS"
+                "multipleDevicesAndHoldersAllowedStatus": "MULTIPLE_HOLDERS"
             }
         ).execute()
         generic_class.update(
@@ -314,7 +314,7 @@ class Command(BaseCommand):
                 "securityAnimation": {
                     "animationType": "foilShimmer"
                 },
-                "multipleDevicesAndHoldersAllowedStatus": "MULTIPLE_USERS"
+                "multipleDevicesAndHoldersAllowedStatus": "MULTIPLE_HOLDERS",
             }
         ).execute()
         transit_class.update(
@@ -337,7 +337,7 @@ class Command(BaseCommand):
                 "securityAnimation": {
                     "animationType": "foilShimmer"
                 },
-                "multipleDevicesAndHoldersAllowedStatus": "MULTIPLE_USERS",
+                "multipleDevicesAndHoldersAllowedStatus": "MULTIPLE_HOLDERS",
                 "customCarriageLabel": {
                     "translatedValues": [{
                         "language": "de",
