fc30f7fd8ff5d9a36c5b27c79296ba7f09509ef6
Q Misell <q@magicalcodewit.ch>
2025-03-26 18:18:36 +0100

    fix sbahn berlin update after login

--- main/saarvv.py
+++ main/saarvv.py
@@ -3,7 +3,10 @@ from . import models, eos, apn
 
 def update_all():
     for account in models.Account.objects.filter(saarvv_device_id__isnull=False):
-        eos.update_eos_tickets(account, "saarvv", "https://saarvv.tickeos.de", "saarvv")
+        update_saarvv_tickets(account)
 
         for t in account.saarvv_tickets.all():
             apn.notify_ticket_if_renewed(t)
+
+def update_saarvv_tickets(account: models.Account):
+    eos.update_eos_tickets(account, "saarvv", "https://saarvv.tickeos.de", "saarvv")
--- main/sbahn_berlin.py
+++ main/sbahn_berlin.py
@@ -2,7 +2,10 @@ from . import models, eos, apn
 
 def update_all():
     for account in models.Account.objects.filter(sbahn_berlin_device_id__isnull=False):
-        eos.update_eos_tickets(account, "sbahn_berlin", "https://sbahn-ber.tickeos.de", "sbb")
+        update_sbahn_berlin_tickets(account)
 
         for t in account.sbahn_berlin_tickets.all():
             apn.notify_ticket_if_renewed(t)
+
+def update_sbahn_berlin_tickets(account: models.Account):
+    eos.update_eos_tickets(account, "sbahn_berlin", "https://sbahn-ber.tickeos.de", "sbb")
